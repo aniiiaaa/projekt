62bb5d69a1856d19c136648403add3b656b31d11
aniiiaaa <ciekawe.korepetycje@onet.pl>
2026-02-11 13:11:48 +0100

    Create logger_main.c

---
+++ logger_main.c
@@ -0,0 +1,27 @@
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+
+#include "struct.h"
+#include "logger.h"
+
+int main(int argc, char **argv){
+    if(argc != 4){
+        fprintf(stderr, "Uzycie: %s id_sem id_shm id_log\n", argv[0]);
+        return 1;
+    }
+
+    int id_s   = atoi(argv[1]);
+    int id_shm = atoi(argv[2]);
+    int logid  = atoi(argv[3]);
+
+    dane *d = shmat(id_shm, NULL, 0);
+    if(d==(void*)-1){
+        perror("shmat");
+        return 1;
+    }
+
+    logger_run(id_s, d, logid);
+    return 0;
+}
